3862ee2757d30670c53c6fd601676de42d27e8ef
Nick wu <387619012@qq.com>
2017-11-03 11:54:54 +0800

    Xheader is ok

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <div v-transfer-dom>
+      <loading ></loading>
+    </div>
+    <div v-transfer-dom>
+      <actionsheet :menus="menus" v-model="showMenu" @on-click-menu="changeLocale"></actionsheet>
+    </div>
       <drawer
         width="200px;"
       :show.sync="drawerVisibility"
@@ -28,7 +34,19 @@
 
       <!-- main content -->
       <view-box ref="viewBox" body-padding-top="46px" body-padding-bottom="55px">
-        
+        <x-header
+          title="bike"
+          :left-options="leftOptions"
+          :right-options="rightOptions"
+          :transition="headerTransition"
+          @on-click-more="onClickMore"
+          style="width:100%;position:absolute;left:0;top:0;z-index:100;"
+        >
+          <span slot="overwrite-left">
+            <x-icon type="navicon" size="35" style="fill:#fff;position:relative;top:-8px;left:-3px;"></x-icon>
+          </span>
+           
+        </x-header>
 
       </view-box>
         
@@ -36,17 +54,27 @@
   </div>
 </template>
 <script>
-import { XHeader,Drawer,Group,Cell,Radio,ViewBox  } from 'vux'
-
+import { Radio, Group, Cell, Badge, Drawer, Actionsheet, ButtonTab, ButtonTabItem, ViewBox, XHeader, Tabbar, TabbarItem, Loading, TransferDom } from 'vux'
+import { mapState, mapActions } from 'vuex'
 export default {
+  directives: {
+    TransferDom
+  },
   name:'index',
   components: {
-    XHeader,
-    Drawer,
+   Radio,
     Group,
     Cell,
-    Radio,
-    ViewBox
+    Badge,
+    Drawer,
+    ButtonTab,
+    ButtonTabItem,
+    ViewBox,
+    XHeader,
+    Tabbar,
+    TabbarItem,
+    Loading,
+    Actionsheet
   },
   data() {
     return {
@@ -79,12 +107,131 @@ export default {
       }, 400)
     },
     onClickMore () {
+     
       this.showMenu = true
     },
     changeLocale (locale) {
       this.$i18n.set(locale)
       this.$locale.set(locale)
     }
+  },
+  computed: {
+    rightOptions () {
+      return {
+        showMore: true
+      }
+    },
+    leftOptions () {
+      return {
+        showBack: true
+      }
+    },
+    headerTransition () {
+      return this.direction === 'forward' ? 'vux-header-fade-in-right' : 'vux-header-fade-in-left'
+    },
   }
 }
 </script>
+<style lang="less">
+@import '~vux/src/styles/reset.less';
+@import '~vux/src/styles/1px.less';
+@import '~vux/src/styles/tap.less';
+body {
+  background-color: #fbf9fe;
+}
+html, body {
+  height: 100%;
+  width: 100%;
+  overflow-x: hidden;
+}
+.demo-icon-22 {
+  font-family: 'vux-demo';
+  font-size: 22px;
+  color: #888;
+}
+.weui-tabbar.vux-demo-tabbar {
+  /** backdrop-filter: blur(10px);
+  background-color: none;
+  background: rgba(247, 247, 250, 0.5);**/
+}
+.vux-demo-tabbar .weui-bar__item_on .demo-icon-22 {
+  color: #F70968;
+}
+.vux-demo-tabbar .weui-tabbar_item.weui-bar__item_on .vux-demo-tabbar-icon-home {
+  color: rgb(53, 73, 94);
+}
+.demo-icon-22:before {
+  content: attr(icon);
+}
+.vux-demo-tabbar-component {
+  background-color: #F70968;
+  color: #fff;
+  border-radius: 7px;
+  padding: 0 4px;
+  line-height: 14px;
+}
+.weui-tabbar__icon + .weui-tabbar__label {
+  margin-top: 0!important;
+}
+.vux-demo-header-box {
+  z-index: 100;
+  position: absolute;
+  width: 100%;
+  left: 0;
+  top: 0;
+}
+@font-face {
+  font-family: 'vux-demo';  /* project id 70323 */
+  src: url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.eot');
+  src: url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.eot?#iefix') format('embedded-opentype'),
+  url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.woff') format('woff'),
+  url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.ttf') format('truetype'),
+  url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.svg#iconfont') format('svg');
+}
+.demo-icon {
+  font-family: 'vux-demo';
+  font-size: 20px;
+  color: #04BE02;
+}
+.demo-icon-big {
+  font-size: 28px;
+}
+.demo-icon:before {
+  content: attr(icon);
+}
+.router-view {
+  width: 100%;
+  top: 46px;
+}
+.vux-pop-out-enter-active,
+.vux-pop-out-leave-active,
+.vux-pop-in-enter-active,
+.vux-pop-in-leave-active {
+  will-change: transform;
+  transition: all 500ms;
+  height: 100%;
+  top: 46px;
+  position: absolute;
+  backface-visibility: hidden;
+  perspective: 1000;
+}
+.vux-pop-out-enter {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
+.vux-pop-out-leave-active {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
+.vux-pop-in-enter {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
+.vux-pop-in-leave-active {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
+.menu-title {
+  color: #888;
+}
+</style>
